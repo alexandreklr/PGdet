c0a01f24af9f08ad11f048d83aa29f41f2c6e12c
Aldric Mercier <160866200+aldricmrcr@users.noreply.github.com>
2024-05-22 17:01:28 +0200

    Update __init__.py j'ai ajouté toutes les fonctions et la version, tout est bon ici normalement

--- src/PGdet/__init__.py
+++ src/PGdet/__init__.py
@@ -1,1 +1,3 @@
-from .PGdet import atom_mapping
+from .PGdet import atom_mapping, count_atoms, calculate_angle, pg_check_smiles, config_mol, molecule_angles, molecule_bonds, plot_molecule, atom_mapping_interface
+
+_version_ = "0.0.1"
