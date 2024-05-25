7d52d24c3b7e6a8482a0e4b101c982bf7523d70a
Migloops <160860083+Migloops@users.noreply.github.com>
2024-05-25 19:30:08 +0200

    Updated test_file.py
    - Finished test_pg_check_smiles
    - Finished test_count_atoms

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,5 +1,4 @@
-from PGdet import atom_mapping, count_atoms, calculate_angle, pg_check_smiles, config_mol, molecule_angles, molecule_bonds
-
+from PGdet import atom_mapping, count_atoms, calculate_angle, pg_check_smiles
 def test_atom_mapping():
   assert atom_mapping('CCO') == 'Cs', 'Test failed : The wrong point group was returned'
   assert atom_mapping('ICl') == 'Cinfv', 'Test failed : The wrong point group was returned'
@@ -18,19 +17,27 @@ def test_atom_mapping():
     pass
 
 def test_count_atoms():
-  pass
+  smiles = ''
+  for i in range(27):
+    assert count_atoms(smiles) == i, 'Test failed : The wrong value was returned.'
+    smiles += 'C'
 
 def test_calculate_angle():
   pass
 
 def test_pg_check_smiles():
-  pass
+  try:
+    test_smiles = ['CC', 'CCO', 'NN', 'c1ccccc1', 'CCCCCCCCCCCCCCCCCCCCCCCCC']
+    for smile in test_smiles:
+      pg_check_smiles(smile)
+  except TypeError:
+    assert False, 'Test failed : Error raised when not expected.'
 
-def test_config_mol():
-  pass
-
-def test_molecule_angles():
-  pass
-
-def test_molecules_bonds():
-  
+  test_smiles = ['not_a_smiles', 'FS(F)(F)(F)(F)F', 'CCCCCCCCCCCCCCCCCCCCCCCCCC', '', 3, ('invalid' , 'set'), {'invalid' : 'dictionary'}, True, '[CH-]1C=CC=C1.[CH-]1C=CC=C1.[Fe+2]']
+  for smile in test_smiles:
+    try :
+      pg_check_smiles(smile)
+      assert False, 'Test failed : Expected error not raised'
+    except TypeError :
+        continue
+    
